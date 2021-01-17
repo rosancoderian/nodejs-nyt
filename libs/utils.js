4e31c5c5e48ebb6a6c05ce19c5c73b6f50cce75d
rosancoderian <rosancoderian@gmail.com>
2021-01-18 02:11:52 +0700

    fix commonErrrorHandler catch wrong error code

--- libs/utils.js
+++ libs/utils.js
@@ -1,8 +1,10 @@
-import { resError500, resErrorUnauthorized } from "./responses.js";
+import { resError500, resErrorUnauthorized, resJSON } from "./responses.js";
 
 export function commonErrorHandler(error, res) {
-  if (error.status == 401) return resErrorUnauthorized(res);
-  if (error.status >= 400 && error.status <= 499)
-    return resJSON(res, res.status, res.data);
+  if (error.response) {
+    if (error.response.status == 401) return resErrorUnauthorized(res);
+    if (error.response.status >= 400 && error.response.status <= 499)
+      return resJSON(res, error.response.status, error.response.data);
+  }
   return resError500(res);
 }
